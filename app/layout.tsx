4315efa10cd52c40e006a5291e7afe3a702416b7
Jocelyn Nemba Pungwe <jocelynpungwe1997@gmail.com>
2024-07-31 22:12:08 -0400

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import Provider from './provider'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Jobify Dev',
+  description: 'Job application tracking system for job hunters',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <Provider>{children}</Provider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
